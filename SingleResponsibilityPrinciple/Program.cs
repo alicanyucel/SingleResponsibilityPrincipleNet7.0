0afd05a1399e541e5f8a8cc13bd79c4315694458
Ali Can Yücel <yucelalican30@gmail.com>
2023-09-21 14:19:31 +0300

    SingleResponsibilityPrinciple
    SingleResponsibilityPrinciple

---
+++ SingleResponsibilityPrinciple/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SingleResponsibilityPrinciple;
+class Database
+{
+    public void Connect()
+    {
+        Console.WriteLine("veri tabanına bağlanıldı.");
+    }
+    public void DisConnect()
+    {
+        Console.WriteLine("veri tabanı bağlantısı kapatıldı");
+    }
+    public string State { get; set; }
+}
+class PersonService
+{
+    public List<Person> GetPersons()
+    {
+        return new()
+        {
+            new() { Name = "ali can", LastName = "yucel"
+            },
+              new() { Name="ali can",LastName="yucel"},
+                new() { Name="veli can",LastName="yüksel"},
+        };
+    }
+}
